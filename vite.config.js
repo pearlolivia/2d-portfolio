1fb2c558b4464a28dc0ded610e8de7fa26641947
pearlolivia <pearlolivia@hotmail.co.uk>
2024-05-29 11:32:22 +0100

    move build files to docs dir

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: './',
-    buiild: {
+    build: {
         minify: 'terser', // kaboom will not work with es modules
+        outDir: './docs',
     },
 });
